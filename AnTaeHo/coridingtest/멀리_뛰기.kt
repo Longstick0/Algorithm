709c4f88f9d043e4ff9c6c1f74d590bdb35796b7
antaeho <an981022@naver.com>
2023-07-04 13:51:02 +0900

    antaeho: 7/4 코테

---
+++ AnTaeHo/coridingtest/멀리_뛰기.kt
@@ -0,0 +1,15 @@
+package coridingtest
+
+class 멀리_뛰기 {
+    fun solution(n: Int): Long {
+        val dp = LongArray(n+1)
+        dp[0] = 1
+        dp[1] = 1
+        if(n < 2)
+            return dp[n]
+        for(i in 2 ..n){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
+        }
+        return dp[n]
+    }
+}
