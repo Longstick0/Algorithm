d9f3e057baf4f84c5c399b9ad70972fc64f8ddfd
Jang DaeYoung <789456jang@naver.com>
2023-07-09 18:15:12 +0900

    jangdaeyoung: 7/9 코테(수리공 항승)

--- JangDaeYoung/boj/A1181.java
+++ JangDaeYoung/boj/A1181.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import static java.lang.System.in;
@@ -15,6 +13,7 @@ import static java.lang.System.out;
  * 단어 정렬
  */
 public class A1181 {
+
     public static void solution() {
     }
 
