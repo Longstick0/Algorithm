59cb672d7d3b4f239c946a20e2e833d6f3f04b45
Jang DaeYoung <789456jang@naver.com>
2023-07-15 03:24:14 +0900

    jangdaeyoung: 7/15 코테(영화감독 숌)

---
+++ JangDaeYoung/boj/A1436.java
@@ -0,0 +1,27 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 영화감독 숌
+ */
+public class A1436 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int num = 666;
+        int count = 1;
+
+        while(count != N) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
