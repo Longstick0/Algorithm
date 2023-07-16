86540d0ee4ec8edc15f24337211cf99d9b816e99
Jang DaeYoung <789456jang@naver.com>
2023-07-16 18:06:50 +0900

    jangdaeyoung: 7/16 코테(팰린드롬 수)

---
+++ JangDaeYoung/boj/A1259.java
@@ -0,0 +1,26 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 팰린드롬 수
+ */
+public class A1259 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            String input = br.readLine();
+            if(Integer.valueOf(input) == 0) break;
+
+            String solve = solve(input);
+            System.out.println(solve);
+        }
+    }
+    private static String solve(String input){
+        StringBuffer sb = new StringBuffer(input);
+        String str = sb.reverse().toString();
+        return input.equals(str) ? "yes" : "no";
+    }
+}
