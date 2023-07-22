46e97304363c7779fd8ab380e627d809d947205b
Jang DaeYoung <789456jang@naver.com>
2023-07-23 01:35:48 +0900

    jangdaeyoung: 7/23 코테(좌표 정렬)

--- JangDaeYoung/algorithm/boj/A11650.java
+++ JangDaeYoung/algorithm/boj/A11650.java
@@ -2,19 +2,43 @@ package algorithm.boj;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
-import static java.lang.System.in;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.IOException;
 
 /**
  * 좌표 정렬하기
  */
 public class A11650 {
+    public static void main(String[] args) throws IOException {
 
-    static int size;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        int N = Integer.parseInt(br.readLine());
 
+        int[][] arr = new int[N][2];
+
+        StringTokenizer st;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i< N ; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+        }
+        System.out.println(sb);
     }
+
 
 }
