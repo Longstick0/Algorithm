445d107244854ca8fbd328baf17b668cdf509ab1
Taaate <an981022@naver.com>
2023-08-21 09:16:01 +0900

    antaeho: 8/21 코테

---
+++ AnTaeHo/boj/boj2869.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.util.Scanner;
+
+public class boj2869 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int up = in.nextInt();		// A
+        int down = in.nextInt();	// B
+        int length = in.nextInt(); 	// C
+
+        int day = (length - down) / (up - down);
+
+        if ((length - down) % (up - down) != 0) {
+            day++;
+        }
+        System.out.println(day);
+    }
+}
