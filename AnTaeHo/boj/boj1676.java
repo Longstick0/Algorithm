7027f3dbcb6abd45bb1c9fa6ff10284d4615ef0f
An Tae ho <an981022@naver.com>
2023-08-24 08:21:56 +0900

    Antaeho (#18)
    * antaeho: 8/21 코테
    
    * antaeho: 8/22 코테

---
+++ AnTaeHo/boj/boj1676.java
@@ -0,0 +1,18 @@
+package boj;
+
+import java.util.Scanner;
+
+public class boj1676 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int num = in.nextInt();
+        int count = 0;
+
+        while (num >= 5) {
+            count += num / 5;
+            num /= 5;
+        }
+        System.out.println(count);
+    }
+}
