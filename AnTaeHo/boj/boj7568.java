d33fe66d16c3ed6b58accb08545d8ecfbee8f173
Taaate <an981022@naver.com>
2023-08-24 10:03:40 +0900

    antaeho: 8/24 코테

---
+++ AnTaeHo/boj/boj7568.java
@@ -0,0 +1,25 @@
+package boj;
+
+import java.util.Scanner;
+
+public class boj7568 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int[][] arr = new int[N][2];
+        for (int i1 = 0; i1 < N; i1++) {
+            arr[i1][0] = in.nextInt();
+            arr[i1][1] = in.nextInt();
+        }
+        for(int i = 0; i < N; i++) {
+            int rank = 1;
+            for(int j = 0; j < N; j++) {
+                if(i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+            System.out.print(rank + " ");
+        }
+    }
+}
